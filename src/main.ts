0adc2f32165ff68bce291e0868411ac9cefb0387
Irene <irene@MacBook-Air-de-Irene.local>
2026-06-12 20:08:08 -0300

    fix: increase request body limit for Garmin webhooks

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import 'reflect-metadata';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { env } from './config/env';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+const REQUEST_BODY_LIMIT = '25mb';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true, bodyParser: false });
+
+  app.use(json({ limit: REQUEST_BODY_LIMIT }));
+  app.use(urlencoded({ extended: true, limit: REQUEST_BODY_LIMIT }));
   app.use(helmet());
   app.enableCors({ origin: env.CORS_ORIGIN });
   app.useGlobalPipes(
